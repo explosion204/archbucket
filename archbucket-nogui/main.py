c4215fc8f0f409f7f83c91e0140806df42099342
explosion204 <dzmitriy20magic@gmail.com>
2020-03-20 21:59:29 +0300

    nogui fix

--- archbucket-nogui/main.py
+++ archbucket-nogui/main.py
@@ -1,43 +1,46 @@
 import re
+import socket
 
 print('ArchBucket | Control App.')
-print('Specify IP-address of ArchBucket server app or leave it empty if it is running localy:')
+print('Specify IP-address of ArchBucket server app or leave it empty if it is running on this machine:')
 ip_pattern = r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$"
 while True:
     ip = input()
     if not ip:
-        ip = 'localhost'
-        port = '8000'
+        ip = socket.gethostbyname(socket.gethostname())
         break
     if not re.match(ip_pattern, ip):
         print('[error] Incorrect IP-address form. Try again: ')
     else:
         break
 
-if ip != 'localhost':
-    print('Specify port to connect to ArchBucket server app.')
-    port_pattern = r"^/d{5}$"
-    while True:
-        port = input()
-        if not re.match(port_pattern, int(port)):
-            print('[error] Incorrect port form. Try again: ')
-        else:
-            break
+print('Specify port to connect to ArchBucket server app.')
+port_pattern = r"^\d{1,5}$"
+while True:
+    port = input()
+    if not re.match(port_pattern, port):
+        print('[error] Incorrect port form. Try again: ')
+    else:
+        break
 
 import socket
+import sys
 from os.path import exists
 try:
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((ip, int(port)))
 except Exception:
     print('[error] Connection refused. Check if provided IP-address and port are available.')
-    import sys
-    sys.exit()
+    sys.exit(0)
 s.close()
 print('[success] Connected to server. Now you can type commands.')
 while True:
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-        connection = sock.connect((ip, int(port)))
+        try:
+            connection = sock.connect((ip, int(port)))
+        except ConnectionRefusedError:
+            print('[error] Connection closed. Server is not available now.')
+            sys.exit(0)
         command = input('>')
         if command:
             if command.startswith('import module'):
@@ -46,12 +49,16 @@ while True:
                     command = ' '.join(command.split()[:3])
                     with open(path, 'r') as f:
                         command += ' ' + f.read()
-            sock.send(command.encode())
-            sock.shutdown(socket.SHUT_WR)
-            response = str()
-            while True:
-                data = sock.recv(1024).decode()
-                if not data:
-                    break
-                response += data
-            print(response)
+            try:
+                sock.send(command.encode())
+                sock.shutdown(socket.SHUT_WR)
+                response = str()
+                while True:
+                    data = sock.recv(1024).decode()
+                    if not data:
+                        break
+                    response += data
+                print(response)
+            except ConnectionResetError:
+                print('[error] Connection closed. Server is not available now.')
+                sys.exit(0)
